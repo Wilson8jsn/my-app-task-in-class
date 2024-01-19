cf910574f122bcd5b52f6bb7f70f9c572bd20e62
Wilson Castillo <dc105677543@gmail.com>
2024-01-19 08:57:18 -0500

    commit

--- App.tsx
+++ App.tsx
@@ -1,14 +1,22 @@
 // App.tsx
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import EcuadorFlagApp from './componets/EcuadorFlagApp';
 
 const App: React.FC = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <EcuadorFlagApp />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default App;
---
+++ componets/EcuadorFlagApp.tsx
@@ -0,0 +1,58 @@
+// EcuadorFlagApp.tsx
+import React, { useState } from 'react';
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+
+const EcuadorFlagApp: React.FC = () => {
+  const [currentColor, setCurrentColor] = useState<string | null>(null);
+
+  const handlePress = (color: string) => {
+    setCurrentColor(color);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => handlePress('yellow')} style={styles.yellow}>
+        <TouchableOpacity onPress={() => handlePress('blue')} style={styles.blue}>
+          <TouchableOpacity onPress={() => handlePress('red')} style={styles.red}>
+            <Text style={styles.text}>{currentColor ? `${currentColor.toUpperCase()}-Ecuador` : 'Ecuador'}</Text>
+          </TouchableOpacity>
+        </TouchableOpacity>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  yellow: {
+    width: 300,
+    height: 200,
+    backgroundColor: '#FFD100',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  blue: {
+    width: 250,
+    height: 150,
+    backgroundColor: '#0033A0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  red: {
+    width: 200,
+    height: 100,
+    backgroundColor: '#EF3340',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 20,
+  },
+});
+
+export default EcuadorFlagApp;
